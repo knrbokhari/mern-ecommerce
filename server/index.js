ac183b53e3836ce62a0751b0d02627c54aed0acb
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-08-29 17:56:51 +0600

    Initialized the server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: "*",
+  methods: "*",
+});
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+server.listen(PORT, () => {
+  console.log("server running at port", PORT);
+});
+
+app.set("socketio", io);
