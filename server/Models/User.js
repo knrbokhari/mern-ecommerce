4bef6e92d569c1c3284f5b6884cb440899d4a0dc
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-08-30 19:59:15 +0600

    creating user Schema

---
+++ server/Models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "is required"],
+    },
+
+    email: {
+      type: String,
+      required: [true, "is required"],
+      unique: true,
+      index: true,
+      validate: {
+        validator: function (str) {
+          return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(str);
+        },
+        message: (props) => `${props.value} is not a valid email`,
+      },
+    },
+
+    password: {
+      type: String,
+      required: [true, "is required"],
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+
+    cart: {
+      type: Object,
+      default: {
+        total: 0,
+        count: 0,
+      },
+    },
+
+    notifications: {
+      type: Array,
+      default: [],
+    },
+
+    orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
+  },
+  { minimize: false }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
