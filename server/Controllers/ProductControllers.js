8e355307ef68d2ffd6ca3e291c2b0f4fd39bd51f
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-09-01 20:05:32 +0600

    creating addProduct route

--- server/Controllers/ProductControllers.js
+++ server/Controllers/ProductControllers.js
@@ -1,5 +1,6 @@
 import Product from "../Models/Product";
 
+//get products
 exports.getProduct = async (req, res) => {
   try {
     const sort = { _id: -1 };
@@ -9,6 +10,23 @@ exports.getProduct = async (req, res) => {
     res.status(400).send(e.message);
   }
 };
-// exports.getProduct = async (req, res) =>{}
+
+//create product
+exports.addProduct = async (req, res) => {
+  try {
+    const { name, description, price, category, images: pictures } = req.body;
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      category,
+      pictures,
+    });
+    const products = await Product.find();
+    res.status(201).json(products);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+};
 // exports.getProduct = async (req, res) =>{}
 // exports.getProduct = async (req, res) =>{}
--- server/Routes/ProductRoutes.js
+++ server/Routes/ProductRoutes.js
@@ -1,8 +1,11 @@
-const { getProduct } = require("../Controllers/ProductControllers");
+const { getProduct, addProduct } = require("../Controllers/ProductControllers");
 
 const router = require("express").Router();
 
 //get products;
 router.get("/", getProduct);
+
+//create product
+router.post("/", addProduct);
 
 module.exports = router;
