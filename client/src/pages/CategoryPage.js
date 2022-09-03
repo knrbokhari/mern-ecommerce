fdd7d4e612d14aaa9dd4d47f0764c9864cb1d052
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-09-04 01:50:25 +0600

    creating Pagination component

--- client/src/pages/CategoryPage.js
+++ client/src/pages/CategoryPage.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Pagination, Row } from "react-bootstrap";
+import { Col, Container, FloatingLabel, Form, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import axios from "../axios";
 import ProductPreview from "../components/ProductPreview";
+import Pagination from "../components/Pagination";
 
 const CategoryPage = () => {
   const { category } = useParams();
@@ -32,16 +33,11 @@ const CategoryPage = () => {
     product.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  function ProductSearch({ _id, category, name, pictures }) {
-    return (
-      <ProductPreview
-        _id={_id}
-        category={category}
-        name={name}
-        pictures={pictures}
-      />
-    );
-  }
+  const ProductSearch = () => {
+    return productsSearch.map((product) => (
+      <ProductPreview key={product._id} product={product} />
+    ));
+  };
 
   return (
     <div className="category-page-container">
@@ -52,27 +48,40 @@ const CategoryPage = () => {
           {category.charAt(0).toUpperCase() + category.slice(1)}
         </h1>
       </div>
-      <div className="filters-container d-flex justify-content-center pt-4 pb-4">
-        <input
+      <div className="filters-container pt-4 pb-4">
+        {/* <input
           type="search"
           placeholder="Search"
           onChange={(e) => setSearchTerm(e.target.value)}
-        />
+          className="mx-auto d-block"
+        /> */}
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Search"
+          className="mx-auto d-block"
+          style={{ width: "300px" }}
+        >
+          <Form.Control
+            type="text"
+            placeholder="Search"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </FloatingLabel>
       </div>
       {productsSearch.length === 0 ? (
         <h1>No products to show</h1>
       ) : (
         <Container>
           <Row>
-            <Col md={{ span: 10, offset: 1 }}>
-              <Pagination
-                data={productsSearch}
-                RenderComponent={ProductSearch}
-                pageLimit={1}
-                dataLimit={5}
-                tablePagination={false}
-              />
-            </Col>
+            {/* <Col md={{ span: 10, offset: 1 }}> */}
+            <Pagination
+              data={productsSearch}
+              RenderComponent={ProductSearch}
+              pageLimit={1}
+              dataLimit={5}
+              tablePagination={false}
+            />
+            {/* </Col> */}
           </Row>
         </Container>
       )}
