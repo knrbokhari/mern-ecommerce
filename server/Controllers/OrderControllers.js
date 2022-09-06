39c83ff909f218ca0cb5501404598d9d499edfd2
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-09-07 01:18:51 +0600

    creating getAllOrder route

--- server/Controllers/OrderControllers.js
+++ server/Controllers/OrderControllers.js
@@ -24,7 +24,14 @@ exports.createOrder = async (req, res) => {
   }
 };
 
-exports.getAllOrder = async (req, res) => {};
+exports.getAllOrder = async (req, res) => {
+  try {
+    const orders = await Order.find().populate("owner", ["email", "name"]);
+    res.status(200).json(orders);
+  } catch (e) {
+    res.status(400).json(e.message);
+  }
+};
 
 exports.shippingOrder = async (req, res) => {};
 
